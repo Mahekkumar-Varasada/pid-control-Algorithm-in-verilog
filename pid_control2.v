cd597c9ce88037fb2d25b53d1002cc0504aa054a
Mahekkumar Varasada <mahekvarasada@gmail.com>
2024-11-03 13:52:44 +0530

    pid_control2.v

--- pid_control2.v
+++ pid_control2.v
@@ -23,10 +23,10 @@ module pid_control2(
     reg sampling_flag = 0;
 
     always @(posedge clk or negedge rst_n) begin
-    //$display("Clock trigered");
+    
         if (~rst_n)
             clk_count <= 16'h0000;
-        else if (clk_count == clk_times) begin // clk_prescaler determines the sampling rate, thus sampling rate would be clk freq/clk_prescaler
+        else if (clk_count == clk_times) begin 
             clk_count <= 16'h0000;
             
             sampling_flag <= 1;
@@ -39,13 +39,14 @@ module pid_control2(
     always @(posedge clk or negedge rst_n) begin
     
         if (~rst_n) begin
-            // Reset logic generally specific to application
+            
         end 
         else if (sampling_flag) begin
                       
             // PID Calculation
             integral <=(Ki * (setpoint - feedback));//error*dt(but at 1 clock cycle)
             derivative <= Kd * ((setpoint - feedback) - prev_error);//(error-prevoius)*dt rate of change of error
+
             // Calculate control signal
             control_signal = (Kp * (setpoint - feedback)) + integral + derivative; 
             prev_error <= (setpoint - feedback);// Update previous error term to feed it for derrivative term.
